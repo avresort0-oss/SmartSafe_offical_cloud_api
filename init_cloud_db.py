9a4ee2844702b5757161ec26027c5c3eaab68486
avresort0 <avresort0@gmail.com>
2026-06-23 01:29:03 -0700

    chore: final project cleanup, removed obsolete scripts and local db

--- init_cloud_db.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv(".env")
-from core.database import init_db
-import logging
-logging.basicConfig(level=logging.INFO)
-print("Initializing database tables...")
-init_db()
-print("Done.")
